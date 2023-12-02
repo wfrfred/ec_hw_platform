59bfc0ce0ec4f1715279f30e014a8986a0d086ba
wfrfred <wfrfred@gmail.com>
2023-12-02 22:13:20 +0800

    fix: remote_data

--- UserCode/Src/CallBack.cpp
+++ UserCode/Src/CallBack.cpp
@@ -5,12 +5,14 @@
 #include "stm32f4xx_hal_uart.h"
 #include "usart.h"
 #include "../Inc/main.h"
+#include "iwdg.h"
 
-extern uint8_t remote_data[18];
+uint8_t remote_data[18];
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size){
     if(huart == &huart3){
         HAL_UARTEx_ReceiveToIdle_DMA(&huart3, remote_data,18);
         remote.handle(remote_data);
+        HAL_IWDG_Refresh(&hiwdg);
     }
 }
