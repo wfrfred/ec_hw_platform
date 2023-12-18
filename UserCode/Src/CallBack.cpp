70c6cde0628cefd25beeed9e6db632622ed4fbc7
wfrfred <wfrfred@gmail.com>
2023-12-18 20:03:36 +0800

    fix: 看门狗忘记开了

--- UserCode/Src/CallBack.cpp
+++ UserCode/Src/CallBack.cpp
@@ -17,7 +17,7 @@ extern Remote remote;
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart){
     if (huart->Instance == huart3.Instance){
-        //HAL_IWDG_Refresh(&hiwdg);
+        HAL_IWDG_Refresh(&hiwdg);
         remote.handle();
         HAL_UART_Receive_DMA(&huart3, remote.remote_data,18);
         HAL_GPIO_WritePin(LED_G_GPIO_Port,LED_G_Pin,GPIO_PIN_SET);
